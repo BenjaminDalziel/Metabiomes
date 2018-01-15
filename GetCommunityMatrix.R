2e72d365591dfcce2d22f25decdeec32494257bf
BenjaminDalziel <benjamin.dalziel@oregonstate.edu>
2018-01-14 20:28:39 -0800

    Update GetCommunityMatrix function

--- GetCommunityMatrix.R
+++ GetCommunityMatrix.R
@@ -1,1 +1,9 @@
-# Return a community interaction matrix sampled per the arguments
+GetCommunityMatrix <- function(S,C,sigma,Pc) {}
+  # Return a community interaction matrix sampled per the arguments
+  #
+  # S       Number of taxa
+  # C       Community connectivity i.e. fraction of nonzero links
+  # sigma   Standard deviation in interaction strength
+  # Pc      Proportion cooperative interactions (+/+)
+  
+
